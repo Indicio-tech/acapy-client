00e4376168ff3911ccaaf76e1806a9437bc86a11
Daniel Bluhm <dbluhm@pm.me>
2021-07-31 16:11:36 -0400

    fix: bug in connection protocol enum
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- acapy_client/models/conn_record_connection_protocol.py
+++ acapy_client/models/conn_record_connection_protocol.py
@@ -2,22 +2,8 @@ from enum import Enum
 
 
 class ConnRecordConnectionProtocol(str, Enum):
-    C = "c"
-    O = "o"
-    N = "n"
-    E = "e"
-    T = "t"
-    I = "i"
-    S = "s"
-    VALUE_7 = "/"
-    VALUE_8 = "1"
-    VALUE_9 = "."
-    VALUE_10 = "0"
-    D = "d"
-    X = "x"
-    H = "h"
-    A = "a"
-    G = "g"
+    CONNECTIONS10 = "connections/1.0"
+    DIDEXCHANGE10 = "didexchange/1.0"
 
     def __str__(self) -> str:
         return str(self.value)
